3ac81328eec3c49c247261b77c99a3b4073fd9c6
Alex <alex.gyumushyan@gmail.com>
2021-06-21 22:27:02 -0400

    added some super basic functionality

--- dequeue.py
+++ dequeue.py
@@ -1,9 +1,52 @@
-#the goal of this project is to create a dequeue with some features
+class Dequeue():
+    def __init__(self):
+        self.queue = []
+        self.max = []
+        self.min = []
 
+    # functions to include:
+    # append
+    # appendleft
+    # clear
+    # copy
+    # count
+    # index
+    # insert
+    # pop
+    # popleft
+    # remove (value)
+    # reverse
+    # rotate
+    # maxlen
+
+    def append(self, val):
+        self.queue.append(val)
+
+    def appendLeft(self, val):
+        self.queue.insert(0, val)
+
+    def print(self):
+        print(*self.queue)
 
 
 def main():
-    print('hello world')
+    dequeue = Dequeue()
+    
+    while True:
+        text = input()
+
+        if text == 'stop':
+            break
+        
+        text = text.split()
+
+
+        if text[0] == 'append':
+            dequeue.append(text[1])
+        elif text[0] == 'appendLeft':
+            dequeue.appendLeft(text[1])
+        elif text[0] == 'print':
+            dequeue.print()
 
 
 if __name__ == '__main__':
